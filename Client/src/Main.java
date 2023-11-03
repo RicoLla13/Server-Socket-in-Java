def8787bf6093e80f5ee872f28e945b3f7330370
liviu-andrei.arsenescu <liviu-andrei.arsenescu@he-arc.ch>
2023-11-03 13:40:01 +0100

    Changes on Server and Client

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -1,7 +1,6 @@
 package Client.src;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -12,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         int port = 3000;
         String ip = "localhost";
-        String exitLine = "exit";
+        String exitLine = "exit()";
 
         Socket client;
 
--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,7 +1,6 @@
 package Server.src;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -12,7 +11,7 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) {
         int port = 3000;
-        String exitLine = "exit";
+        String exitLine = "exit()";
 
         ServerSocket server;
         Socket client;
@@ -29,7 +28,7 @@ public class Main {
             PrintWriter clWriter = new PrintWriter(clOutStr, true);
 
             String line1 = "Client connected succesfully!\n";
-            String line2 = "Type exit to close connection.";
+            String line2 = "Type " + exitLine + " to close connection.";
 
             String line = line1 + line2;
             clWriter.println(line);
